5a9ae2f96d38103592dd90b4a2a8daf771d4888b
Summer <summer.d.howard@gmail.com>
2019-06-05 16:15:30 -0500

    initial commit

---
+++ DefaultValuesCSharp/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+    public class Class1
+    {
+        static String location;  
+        static DateTime time;
+        static int integer;
+        static sbyte sbyteVariable;
+        static byte byteVariable;
+        static long longVariable;
+        static short shortVariable;
+        static char charVariable;
+        static bool boolVariable;
+
+
+    public static void Main()
+        {    
+        Console.WriteLine(location == null ? "location is null" : location);
+        Console.WriteLine(time == null ? "time is null" : time.ToString());
+
+
+        Console.WriteLine("sbyteVariable :: " + sbyteVariable);
+
+        Console.WriteLine("byteVariable :: " + byteVariable);
+        Console.WriteLine("integer :: " + integer);
+        Console.WriteLine("shortVariable :: " + shortVariable);
+        Console.WriteLine("longVariable :: " + longVariable);
+
+        Console.WriteLine("charVariable :: '" + charVariable + "'");
+        Console.WriteLine("boolVariable :: " + boolVariable);
+
+        }
+
+      
+
+    }
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+There are two kinds of types in C#: value types and reference types. 
+
+Variables of value types directly contain their data
+
+Reference types are objects
+
+With reference types, it is possible for two variables to reference the same object and thus possible for operations on one variable to affect the object referenced by the other variable. 
+
+Character and string processing in C# uses Unicode encoding. The char type represents a UTF-16 code unit, and the string type represents a sequence of UTF-16 code units.
