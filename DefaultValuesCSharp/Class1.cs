e0989cb489e3739706373b5843f88bd5bf226f07
Summer <summer.d.howard@gmail.com>
2019-06-13 11:24:03 -0500

    adding pass by value and refence examples

--- DefaultValuesCSharp/Class1.cs
+++ DefaultValuesCSharp/Class1.cs
@@ -2,8 +2,9 @@
 
     public class Class1
     {
-        static String location;  
+        static String stringVariable;  
         static DateTime time;
+
         static int integer;
         static sbyte sbyteVariable;
         static byte byteVariable;
@@ -13,25 +14,40 @@
         static bool boolVariable;
 
 
+    public class Person
+    {
+        public string name { get; set; }
+    }
+
+    public static void Mutate(Person person)
+    {
+        person.name = "Mutated";
+    }
+
+
     public static void Main()
         {    
-        Console.WriteLine(location == null ? "location is null" : location);
+        Console.WriteLine(stringVariable == null ? "stringVariable is null" : stringVariable);
         Console.WriteLine(time == null ? "time is null" : time.ToString());
 
-
         Console.WriteLine("sbyteVariable :: " + sbyteVariable);
-
         Console.WriteLine("byteVariable :: " + byteVariable);
         Console.WriteLine("integer :: " + integer);
         Console.WriteLine("shortVariable :: " + shortVariable);
         Console.WriteLine("longVariable :: " + longVariable);
-
         Console.WriteLine("charVariable :: '" + charVariable + "'");
         Console.WriteLine("boolVariable :: " + boolVariable);
 
-        }
 
-      
+
+        var person = new Person() { name = "Default"};
+        Console.WriteLine(person.name);
+        Mutate(person);
+        Console.WriteLine(person.name);
 
     }
+
+
+
+}
 
